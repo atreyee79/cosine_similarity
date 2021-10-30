a9adefe586defc64d949b2fb3301eade7a1ca69f
atreyee79 <80617664+atreyee79@users.noreply.github.com>
2021-10-31 00:35:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def output():
     else :
         simi='not similar'   
     return render_template("home.html",predict= {simi}) 
-
-app.run(debug=True)    
+if __name__=='__main__':
+    app.run(debug=True)    
